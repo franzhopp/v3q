825d46d3f7f6428f3c07c88e3d565b9543708758
louohs <louisa.hadjili@epitech.eu>
2023-09-05 11:25:13 +0200

    [ADD]

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -209,7 +209,7 @@ const Navbar = () => {
           <motion.span
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 3, delay: 0.5 }}
+            transition={{ duration: 5, delay: 0.5 }}
             className="block xl:inline"
           >
             Agence digitale
