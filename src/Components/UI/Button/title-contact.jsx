64ff42185005067e3347daf67d6c436505d8c036
louohs <louisa.hadjili@epitech.eu>
2023-09-05 10:39:27 +0200

    [ADD] refactor paragraphe défilant

--- src/Components/UI/Button/title-contact.jsx
+++ src/Components/UI/Button/title-contact.jsx
@@ -2,7 +2,7 @@ const Title = () => {
   return (
     <div className="image-navi-studio-contact">
       <div
-        className="pt-20 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold text-5xl sm:text-8xl"
+        className="pt-20 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold text-4xl sm:text-8xl"
         data-aos="fade-right"
       >
         <p className="">Contactez-nous</p>
