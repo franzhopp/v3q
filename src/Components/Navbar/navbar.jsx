24fdc56cc9c5b7f7a3521f5c30caeaaa094757e0
louohs <louisa.hadjili@epitech.eu>
2023-09-06 22:25:04 +0200

    [ADD] responsive img nav tablet/mobile refactor

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
         <div className="container mt-2 mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-24">
             <div className="flex items-center">
-              <img className="h-20 bg-cover bg-no-repeat" src={Image} />
+              <img className="lg:h-20 h-10 bg-cover bg-no-repeat" src={Image} />
             </div>
             <div className="flex items-center">
               <NavLink
