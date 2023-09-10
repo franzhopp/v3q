46cf407b8e6cddb7fc39e03f88328e5269360d27
louohs <louisa.hadjili@epitech.eu>
2023-09-10 14:54:00 +0200

    [ADD] update

--- src/Components/UI/Button/button.jsx
+++ src/Components/UI/Button/button.jsx
@@ -36,7 +36,6 @@ const Button = () => {
           to="/"
           href="about"
           onClick={SectionToScroll}
-          data-aos="fade-right"
           className="inline-flex items-center gap-2 border rounded-full bg-FFF6E4 px-8 py-3 text-black"
         >
           <span className="text-3xl mr-3 font-semibold heading-responsive">
