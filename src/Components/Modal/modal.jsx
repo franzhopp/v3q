020d1b760965164e78c2b69ece6e63d317c8e8bb
louohs <louisa.hadjili@epitech.eu>
2023-09-11 19:21:37 +0200

    [ADD] navbar finished

--- src/Components/Modal/modal.jsx
+++ src/Components/Modal/modal.jsx
@@ -25,7 +25,7 @@ const ModalSearch = ({ closeModal, searchQuery }) => {
           placeholder="Votre recherche..."
           onChange={handleSearch}
           onKeyUp={handleEnterPress}
-          className="rounded-xl p-3 outline-none border border-1-C22E2E input-bordered input-primary mb-5 ml-3 mr-3 sm:mb-0 md:ml-0 "
+          className="rounded-xl p-3 outline-none border border-1-C22E2E input-bordered input-primary mb-5 ml-3 mr-3 sm:mb-0 md:ml-0"
         />
         <button onClick={() => closeModal(false)}>
           <ImCross className="text-C22E2E mb-4 sm:mb-0" />
