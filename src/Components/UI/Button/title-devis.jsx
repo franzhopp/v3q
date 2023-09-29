c96c2efaa350c15e9d6e187584ace6c3891bcc63
louohs <louisa.hadjili@epitech.eu>
2023-09-29 17:45:13 +0200

    [ADD] portfolio remake

--- src/Components/UI/Button/title-devis.jsx
+++ src/Components/UI/Button/title-devis.jsx
@@ -1,8 +1,8 @@
 const TitleDevis = () => {
   return (
-    <div className="image-navi-studio-contact pb-24">
+    <div className="image-navi-studio-contact pb-24 pt-12 sm:pt-0">
       <div
-        className="pt-24 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold"
+        className="pt-28 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold"
         data-aos="fade-left"
       >
         <p className="mt-4 text-4xl sm:text-6xl">Devis Gratuit</p>
