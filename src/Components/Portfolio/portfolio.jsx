ea158ceb2c8ce6e2cc490a812cd6ec9001c3b4b8
louohs <louisa.hadjili@epitech.eu>
2023-09-29 04:05:16 +0200

    [ADD] page portfolio

--- src/Components/Portfolio/portfolio.jsx
+++ src/Components/Portfolio/portfolio.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ButtonSend from "../UI/Button/button-send";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -7,8 +7,43 @@ import NavbarDevis from "../Navbar/NavbarDevis/d-navbar";
 import { NavLink } from "react-router-dom";
 import Pattern from "../../assets/pattern.png";
 import TitlePorfolio from "../UI/Button/title-porfolio";
+import Image1 from "../../assets/cards.png";
+import ImgMade from "../../assets/madewithlove.png";
 
 const Portfolio = () => {
+  const cards = [
+    {
+      image: Image1,
+      title: "By Sarah Cuisine",
+      content:
+        "Nous avons l'heureux de vous annoncer que le site By Sarah Cuisine a été créé par la société navi studio.™ Développé et designé par Louisa, ce projet a été riche en développement car il permet à Sarah de promouvoir sa marque influente en tant que traiteur.",
+    },
+    {
+      image: Image1,
+      title: "Test",
+      content:
+        "Nous avons l'heureux de vous annoncer que le site By Sarah Cuisine a été créé par la société navi studio.™ Développé et designé par Louisa, ce projet a été riche en développement car il permet à Sarah de promouvoir sa marque influente en tant que traiteur.",
+    },
+    {
+      image: Image1,
+      title: "Test",
+      content:
+        "Nous avons l'heureux de vous annoncer que le site By Sarah Cuisine a été créé par la société navi studio.™ Développé et designé par Louisa, ce projet a été riche en développement car il permet à Sarah de promouvoir sa marque influente en tant que traiteur.",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + cards.length) % cards.length
+    );
+  };
+
   useEffect(() => {
     AOS.init({
       once: true,
@@ -20,9 +55,48 @@ const Portfolio = () => {
       <div>
         <TitlePorfolio />
       </div>
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-        <div className="sm:h-auto w-52">
-          <img src={Pattern} className="" />
+      <div className="mb-10 mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        {/* <div className="sm:h-auto">
+          <img src={Pattern} className="h-auto w-32" />
+        </div> */}
+        <div className="flex justify-center mt-10">
+          <div className="p-6  h-1/2 w-96 rounded-xl shadow-xl">
+            <div className="mt-4">
+              <img
+                src={cards[currentIndex].image}
+                alt={cards[currentIndex].title}
+                className="w-full h-60 object-cover rounded"
+              />
+            </div>
+            <p className="mt-6 text-black text-xl font-extrabold ">
+              <span className="text-E18AB0 font-bold number">
+                {currentIndex + 1}.
+              </span>{" "}
+              {cards[currentIndex].title}
+            </p>
+            <div className="">
+              <p className="text-black overflow-auto p-5 h-40">
+                {cards[currentIndex].content}
+              </p>
+            </div>
+            <div className="flex justify-center mt-4">
+              <button
+                className="bg-black font-extrabold text-FFF6E4 px-4 py-2 rounded-full mr-2"
+                onClick={handlePrev}
+              >
+                Précédent
+              </button>
+              <button
+                className="bg-black font-extrabold text-FFF6E4 px-4 py-2 rounded-full"
+                onClick={handleNext}
+              >
+                Suivant
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center mt-10">
+          <img src={ImgMade} alt="Icône made with love" className="button-design-formbounce animate-bounce-bounce" />
         </div>
       </div>
       <Footer />
--- src/Components/UI/Button/title-porfolio.jsx
+++ src/Components/UI/Button/title-porfolio.jsx
@@ -1,8 +1,8 @@
 const TitlePorfolio = () => {
   return (
-    <div className="image-navi-studio-porfolio pb-24">
+    <div className="image-navi-studio-porfolio pb-24 pt-12 sm:pt-0">
       <div
-        className="pt-32 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold"
+        className="pt-28 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold"
         data-aos="fade-left"
       >
         <p className="mt-4 text-4xl sm:text-6xl">Portfolio</p>
