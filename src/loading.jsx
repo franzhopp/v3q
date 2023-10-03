46073c969c66a140846cbc8e81d0e687a1be46fa
louohs <louisa.hadjili@epitech.eu>
2023-10-03 23:58:56 +0200

    [ADD] page loader

--- src/loading.jsx
+++ src/loading.jsx
@@ -2,8 +2,8 @@ import Logo from "./assets/logo-bit.png";
 
 const Loader = () => {
   return (
-    <div className="bg-C22E2E flex justify-center h-screen pt-72">
-      <img src={Logo} className="animate-pulse h-16 w-16" />
+    <div className="bg-C22E2E flex justify-center h-screen pt-80">
+      <img src={Logo} className="animate-spin h-16 w-16" />
     </div>
   );
 };
