ef67619c749eb2335f7d86dfc193f7d6bb9a3cc5
louohs <louisa.hadjili@epitech.eu>
2023-10-07 17:10:10 +0200

    [ADD] update logo loading navistudio

--- src/loading.jsx
+++ src/loading.jsx
@@ -1,10 +1,19 @@
 import Logo from "./assets/logo-bit.png";
+import LogoNaviStudio from "./assets/logo-5.png";
 
 const Loader = () => {
   return (
-    <div className="bg-FFF6E4 flex justify-center h-screen pt-80">
-      <img src={Logo} className="animate-pulse h-16 w-16" />
-    </div>
+    <>
+      <div className="bg-FFF6E4 flex justify-center h-screen pt-80">
+        <img src={Logo} className="animate-pulse h-16 w-16" />
+      </div>
+      <div className=" flex justify-center">
+        <img
+          src={LogoNaviStudio}
+          className="animate-bounce h-10 w-auto sm:h-10"
+        />
+      </div>
+    </>
   );
 };
 
