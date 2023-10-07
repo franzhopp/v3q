fa891ad349fa5d4c666301570c219fc42c0a98b2
louohs <louisa.hadjili@epitech.eu>
2023-10-07 02:02:41 +0200

    [ADD] change refactor loader

--- src/loading.jsx
+++ src/loading.jsx
@@ -4,7 +4,6 @@ const Loader = () => {
   return (
     <div className="bg-FFF6E4 flex justify-center h-screen pt-80">
       <img src={Logo} className="animate-pulse h-16 w-16" />
-      <div className="loader"></div>
     </div>
   );
 };
