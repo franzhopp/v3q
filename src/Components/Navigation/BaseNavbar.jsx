2d41262d4c6c7cb999a493ac0d7b762ba0651bca
louohs <louisa.hadjili@epitech.eu>
2023-10-07 16:30:15 +0200

    [ADD] change devis/contact refactor

--- src/Components/Navigation/BaseNavbar.jsx
+++ src/Components/Navigation/BaseNavbar.jsx
@@ -17,9 +17,6 @@ const Navbar = () => {
   const [isMouseOnSubmenu, setIsMouseOnSubmenu] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
-
-  
-
   // Toggle → HandleMouse from list items
   const toggleMenuMobile = () => {
     setToggleMenu(!toggleMenu);
@@ -548,7 +545,7 @@ const Navbar = () => {
         </ul>
       </nav>
 
-      <div className="pt-56 text-center sm:mt-28 sm:pt-52">
+      <div className="bg-C22E2E pt-56 text-center sm:mt-28 sm:pt-52">
         <h1 className="text-normal text-4xl tracking-tight font-extrabold text-FFF6E4 sm:text-8xl md:text-8xl">
           <motion.span
             id="all"
