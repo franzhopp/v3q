52b004a398cd836e7c973f8f3b9babefcbbe8691
louohs <louisa.hadjili@epitech.eu>
2023-10-08 02:44:26 +0200

    [ADD] change title portfolio

--- src/Components/UI/Title/TitleFormDevis.jsx
+++ src/Components/UI/Title/TitleFormDevis.jsx
@@ -5,7 +5,7 @@ const TitleDevis = () => {
         className="pt-28 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold"
         data-aos="fade-left"
       >
-        <p className="mt-4 text-4xl sm:text-6xl">Devis Gratuit</p>
+        <p className="mt-4 mr-3 text-4xl sm:text-6xl">Devis Gratuit</p>
       </div>
     </div>
   );
--- src/Components/UI/Title/TitlePorfolio.jsx
+++ src/Components/UI/Title/TitlePorfolio.jsx
@@ -5,7 +5,7 @@ const TitlePorfolio = () => {
         className="pt-28 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold"
         data-aos="fade-left"
       >
-        <p className="mt-4 text-4xl sm:text-6xl">Nos réalisations</p>
+        <p className="mt-4 mr-3 text-4xl sm:text-6xl">Nos réalisations</p>
       </div>
     </div>
   );
