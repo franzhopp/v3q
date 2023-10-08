7e41800ac5592eaa595bb8853dfd4caa5e910308
louohs <louisa.hadjili@epitech.eu>
2023-10-09 01:51:59 +0200

    [ADD] rework loader enter

--- src/loading.jsx
+++ src/loading.jsx
@@ -4,8 +4,8 @@ import LogoNaviStudio from "./assets/logo-5.png";
 const Loader = () => {
   return (
     <>
-      <div className="bg-C22E2E flex justify-center h-screen pt-80">
-        <img src={Logo} className="animate-spin h-14 w-14" />
+      <div className="bg-C22E2E flex flex-col justify-center items-center h-screen">
+        <img src={Logo} className="animate-pulse h-14 w-14" />
       </div>
     </>
   );
