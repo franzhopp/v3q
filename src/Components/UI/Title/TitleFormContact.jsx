aa97e899f9b6a6d17fdf83c7bced6f1acb38925c
louohs <louisa.hadjili@epitech.eu>
2023-10-08 02:47:47 +0200

    [ADD] change title portfolio

--- src/Components/UI/Title/TitleFormContact.jsx
+++ src/Components/UI/Title/TitleFormContact.jsx
@@ -5,7 +5,7 @@ const Title = () => {
         className="pt-20 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold"
         data-aos="fade-left"
       >
-        <p className="mt-4 mr-3 text-4xl sm:text-6xl">
+        <p className="mt-4 mr-5 text-4xl sm:text-6xl">
           Contactez-nous
         </p>
       </div>
