c8e3f265a20a63c5ff09e047f8edfc877bc533a4
louohs <louisa.hadjili@epitech.eu>
2023-10-09 01:59:47 +0200

    [ADD] rework loader enter

--- src/Components/UI/Title/TitlePorfolio.jsx
+++ src/Components/UI/Title/TitlePorfolio.jsx
@@ -5,7 +5,7 @@ const TitlePorfolio = () => {
         className="pt-28 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold"
         data-aos="fade-left"
       >
-        <p className="mt-4 mr-5 text-4xl sm:text-6xl">Nos réalisations</p>
+        <p className="mt-4 mr-5 text-4xl sm:text-6xl">Portfolio</p>
       </div>
     </div>
   );
--- src/loading.jsx
+++ src/loading.jsx
@@ -1,10 +1,9 @@
 import Logo from "./assets/logo-bit.png";
-import LogoNaviStudio from "./assets/logo-5.png";
 
 const Loader = () => {
   return (
     <>
-      <div className="bg-C22E2E flex flex-col justify-center items-center h-screen">
+      <div className="bg-C22E2E flex flex-col justify-center items-center pt-96">
         <img src={Logo} className="animate-pulse h-14 w-14" />
       </div>
     </>
