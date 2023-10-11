e8fca9062418f971c315ff4e48a043eae0efe4be
louohs <louisa.hadjili@epitech.eu>
2023-10-11 02:53:20 +0200

    [ADD] rework search reload

--- src/Components/Modal/SearchModal.jsx
+++ src/Components/Modal/SearchModal.jsx
@@ -16,6 +16,7 @@ const ModalSearch = ({ closeModal, searchQuery }) => {
       );
       if (matchingPage) {
         navigate(matchingPage.link);
+        window.location.reload();
         closeModal(false);
       }
     }
@@ -34,9 +35,9 @@ const ModalSearch = ({ closeModal, searchQuery }) => {
         onKeyDown={handleSearch}
         value={searchQueryText}
         onChange={(e) => setSearchQueryText(e.target.value)}
-        className="rounded-xl p-3 outline-none border border-1-C22E2E input-bordered input-primary mb-5 ml-3 mr-3 sm:mb-0 md:ml-0"
+        className="rounded-xl p-3 outline-none border border-1-C22E2E input-bordered input-primary mb-5 ml-7 mr-3 sm:mb-0 md:ml-0"
       />
-      <button onClick={handleReset}>
+      <button onClick={handleSearch}>
         <ImCross
           onClick={() => closeModal(false)}
           className="text-C22E2E mb-4 sm:mb-0"
