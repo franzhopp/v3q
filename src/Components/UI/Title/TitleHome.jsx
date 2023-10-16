05e4e650a9e5f8b9ad0fd046f74340c55fa59b35
louohs <louisa.hadjili@epitech.eu>
2023-10-16 17:49:42 +0200

    [ADD] update page height

--- src/Components/UI/Title/TitleHome.jsx
+++ src/Components/UI/Title/TitleHome.jsx
@@ -5,20 +5,24 @@ const TitleHome = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center h-my-screen">
-        <h1 className="font-extrabold tracking-tight text-FFF6E4 text-4xl sm:text-8xl md:text-8xl">
-          <motion.span
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 0.5 }}
-          >
-            Agence digitale
-          </motion.span>
-        </h1>
-        <span className="font-extrabold text-FFF6E4">PARIS X LONDON</span>
+        <div className="h-10">
+          <h1 className="font-extrabold tracking-tight text-FFF6E4 text-4xl sm:text-8xl md:text-8xl">
+            <motion.span
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.5 }}
+            >
+              Agence digitale
+            </motion.span>
+          </h1>
+          <div className="flex justify-center">
+            <span className="font-extrabold text-FFF6E4">PARIS X LONDON</span>
+          </div>
+        </div>
       </div>
-
-      <ButtonDiscover />
-
+      <div className="h-auto mb-10">
+        <ButtonDiscover />
+      </div>
       <div className="h-20">
         <p className="mt-10">
           <span className="whitespace-nowrap animation-scrolling-rtl text-black text-2xl sm:text-4xl">
