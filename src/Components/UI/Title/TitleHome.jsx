c059f9c3aff5bc17892a4c2966895c09dba3f428
louohs <louisa.hadjili@epitech.eu>
2023-10-17 00:08:01 +0200

    [ADD] change home

--- src/Components/UI/Title/TitleHome.jsx
+++ src/Components/UI/Title/TitleHome.jsx
@@ -4,7 +4,7 @@ import ButtonDiscover from "../Button/ButtonDiscover";
 const TitleHome = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-my-screen pt-0 sm:pt-20">
+      <div className="flex flex-col justify-center items-center h-my-screen">
         <h1 className="font-extrabold tracking-tight text-FFF6E4 text-4xl sm:text-8xl md:text-8xl">
           <motion.span
             initial={{ opacity: 0, y: 20 }}
@@ -15,24 +15,18 @@ const TitleHome = () => {
           </motion.span>
         </h1>
         <div className="flex justify-center">
-          <span
-            data-aos="fade-right"
-            className="font-extrabold text-sm text-C22E2E"
-          >
+          <span data-aos="fade-right" className="font-extrabold text-FFF6E4">
             PARIS X LONDON
           </span>
         </div>
       </div>
-      <div className="pt-10 pb-24">
-        <ButtonDiscover />
-      </div>
-      <div className="h-28">
-        <p className="mt-20">
-          <span className="whitespace-nowrap animation-scrolling-rtl text-black text-2xl sm:text-4xl">
-            DESIGN | DÉVELOPPEMENT | MARKETING DIGITAL | STRATÉGIE DE MARQUE
-          </span>
-        </p>
-      </div>
+
+      <ButtonDiscover />
+      <p className="pt-14 pb-10">
+        <span className="whitespace-nowrap animation-scrolling-rtl text-black text-2xl sm:text-4xl">
+          DESIGN | DÉVELOPPEMENT | MARKETING DIGITAL | STRATÉGIE DE MARQUE
+        </span>
+      </p>
     </>
   );
 };
