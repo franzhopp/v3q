e9bb61dcfbb06e9f51be1188e929aee72eac28ac
louohs <louisa.hadjili@epitech.eu>
2023-10-16 21:59:49 +0200

    [ADD] change animation btn

--- src/Components/UI/Title/TitleHome.jsx
+++ src/Components/UI/Title/TitleHome.jsx
@@ -4,7 +4,7 @@ import ButtonDiscover from "../Button/ButtonDiscover";
 const TitleHome = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-my-screen pt-0 sm:pt-28">
+      <div className="flex flex-col justify-center items-center h-my-screen pt-0 sm:pt-32">
         <h1 className="font-extrabold tracking-tight text-FFF6E4 text-4xl sm:text-8xl md:text-8xl">
           <motion.span
             initial={{ opacity: 0, y: 20 }}
@@ -20,13 +20,13 @@ const TitleHome = () => {
           </span>
         </div>
       </div>
-      <div className="pt-20">
+      <div className="">
         <div className="h-44">
           <ButtonDiscover />
         </div>
       </div>
       <div className="h-20">
-        <p className="mt-8">
+        <p className="mt-10">
           <span className="whitespace-nowrap animation-scrolling-rtl text-black text-2xl sm:text-4xl">
             DESIGN | DÉVELOPPEMENT | MARKETING DIGITAL | STRATÉGIE DE MARQUE
           </span>
