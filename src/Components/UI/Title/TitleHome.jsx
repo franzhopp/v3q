87da3c7408fb7a7709cad6d13b11930f2d2e31f7
louohs <louisa.hadjili@epitech.eu>
2023-10-16 18:10:56 +0200

    [ADD] update page height vh

--- src/Components/UI/Title/TitleHome.jsx
+++ src/Components/UI/Title/TitleHome.jsx
@@ -5,19 +5,17 @@ const TitleHome = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center h-my-screen">
-        <div className="h-10">
-          <h1 className="font-extrabold tracking-tight text-FFF6E4 text-4xl sm:text-8xl md:text-8xl">
-            <motion.span
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1, delay: 0.5 }}
-            >
-              Agence digitale
-            </motion.span>
-          </h1>
-          <div className="flex justify-center">
-            <span className="font-extrabold text-FFF6E4">PARIS X LONDON</span>
-          </div>
+        <h1 className="font-extrabold tracking-tight text-FFF6E4 text-4xl sm:text-8xl md:text-8xl">
+          <motion.span
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            Agence digitale
+          </motion.span>
+        </h1>
+        <div className="flex justify-center">
+          <span className="font-extrabold text-FFF6E4">PARIS X LONDON</span>
         </div>
       </div>
       <ButtonDiscover />
