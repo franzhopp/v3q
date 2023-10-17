145c69564018105fa386e4602c5996f0df8a87de
louohs <louisa.hadjili@epitech.eu>
2023-10-17 16:45:21 +0200

    [ADD] update homepage

--- src/Components/UI/Title/TitleHome.jsx
+++ src/Components/UI/Title/TitleHome.jsx
@@ -4,31 +4,35 @@ import ButtonDiscover from "../Button/ButtonDiscover";
 const TitleHome = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-my-screen pt-20">
-        <h1 className="font-extrabold tracking-tight text-FFF6E4 text-4xl sm:text-8xl md:text-8xl">
-          <motion.span
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 0.5 }}
-          >
-            Agence digitale
-          </motion.span>
-        </h1>
-        <div className="flex justify-center">
-          <span
-            data-aos="fade-right"
-            className="font-extrabold text-FFF6E4 text-sm"
-          >
-            PARIS X LONDON
-          </span>
+      <div className="">
+        <div className="flex flex-col justify-center items-center h-my-screen pt-60 pb-80">
+          <h1 className="font-extrabold tracking-tight text-FFF6E4 text-4xl sm:text-8xl md:text-8xl">
+            <motion.span
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.5 }}
+            >
+              Agence digitale
+            </motion.span>
+          </h1>
+          <div className="flex justify-center">
+            <span
+              data-aos="fade-right"
+              className="font-extrabold text-FFF6E4 text-sm"
+            >
+              PARIS X LONDON
+            </span>
+          </div>
         </div>
+        <div className="flex justify-center sm:justify-end px-0 sm:px-20 pt-10">
+          <ButtonDiscover />
+        </div>
+        <p className="pt-10 pb-10">
+          <span className="whitespace-nowrap animation-scrolling-rtl text-black text-2xl sm:text-4xl">
+            DESIGN | DÉVELOPPEMENT | MARKETING DIGITAL | STRATÉGIE DE MARQUE
+          </span>
+        </p>
       </div>
-      <ButtonDiscover />
-      <p className="pt-14 pb-14">
-        <span className="whitespace-nowrap animation-scrolling-rtl text-black text-2xl sm:text-4xl">
-          DESIGN | DÉVELOPPEMENT | MARKETING DIGITAL | STRATÉGIE DE MARQUE
-        </span>
-      </p>
     </>
   );
 };
