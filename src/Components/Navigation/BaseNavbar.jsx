142cab82f0838caa2694a05a06d0e5e0e8ba712c
louohs <louisa.hadjili@epitech.eu>
2023-10-19 18:27:52 +0200

    [ADD] change py basenavbar

--- src/Components/Navigation/BaseNavbar.jsx
+++ src/Components/Navigation/BaseNavbar.jsx
@@ -531,7 +531,7 @@ const Navbar = () => {
                   >
                     <NavLink
                       to={val.link}
-                      className="block text-C22E2E border-C22E2E border-b-2 hover:text-0C3E78 px-4 py-1 font-extrabold"
+                      className="block text-C22E2E border-C22E2E border-b-2 hover:text-0C3E78 px-4 py-2 font-extrabold"
                     >
                       {val.lng}
                     </NavLink>
