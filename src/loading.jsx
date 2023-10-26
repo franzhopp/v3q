e9e9b706f8982c0068ab64b18e783d9aa2d7aeca
louohs <louisa.hadjili@epitech.eu>
2023-10-26 21:55:14 +0200

    [UPDATE] loader animate logo

--- src/loading.jsx
+++ src/loading.jsx
@@ -30,8 +30,8 @@ const Loader = () => {
   return (
     <div className="bg-C22E2E flex flex-col justify-center items-center h-screen">
       <img src={Logo} className="h-14 w-auto mr-1 animate-pulse" />
-      <div className="flex justify-center mt-5">
-        <img src={TitleLogo} className="h-3 animate-formbounce" />
+      <div className="flex justify-center mt-3">
+        <img src={TitleLogo} className="h-3 ml-1 animate-formbounce" />
       </div>
       {/* <div className="flex justify-center mt-10 font-gexo text-FFF6E4">
           <div data-aos="fade-right" className="mr-1">
