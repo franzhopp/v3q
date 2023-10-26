147cfebb32b4759e51926ebf05d9585ee06f4962
louohs <louisa.hadjili@epitech.eu>
2023-10-27 00:41:53 +0200

    [ADD] dl js-cookie lib and work cookie website

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -4,8 +4,22 @@ import SecondSection from "../Section/SecondSection.jsx";
 import SectionAgency from "../Section/SectionAgency.jsx";
 import Form from "../Contact/FormContact.jsx";
 import Footer from "../Footer/Footer.jsx";
+import CookieModal from "../Cookie/Cookie.jsx";
+import { useState } from "react";
 
 const Home = () => {
+  const [isCookieModalOpen, setIsCookieModalOpen] = useState(true); // Vous pouvez initialiser ceci comme vous le souhaitez
+
+  // Fonction pour gérer l'acceptation des cookies
+  const handleAcceptCookies = () => {
+    setIsCookieModalOpen(false); // Ferme le modal lorsque les cookies sont acceptés
+    // Vous pouvez également gérer le stockage des cookies ici
+  };
+
+  // Fonction pour gérer la fermeture du modal sans accepter les cookies
+  const handleRequestClose = () => {
+    setIsCookieModalOpen(false);
+  };
   return (
     <>
       <Header />
@@ -13,6 +27,13 @@ const Home = () => {
       <SecondSection />
       <SectionAgency />
       <Form />
+      {isCookieModalOpen && (
+        <CookieModal
+          isOpen={isCookieModalOpen}
+          onRequestClose={handleRequestClose}
+          onAccept={handleAcceptCookies}
+        />
+      )}
       <Footer />
     </>
   );
