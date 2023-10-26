388dd95f5c12b1482145befbeca708d1d205ddd4
louohs <louisa.hadjili@epitech.eu>
2023-10-26 23:13:36 +0200

    [UPDATE] loader animate logo

--- src/loading.jsx
+++ src/loading.jsx
@@ -28,47 +28,14 @@ const Loader = () => {
   // const animationConfig3 = [{ dataAos: "fade-left", dataAosDelay: 100 }];
 
   return (
-    <div className="bg-C22E2E flex flex-col justify-center items-center h-screen">
-      <img src={Logo} className="h-14 w-auto mr-1 animate-pulse" />
-      <div className="flex justify-center mt-3">
-        <img src={TitleLogo} className="h-3 ml-2 animate-formbounce" />
+    <section>
+      <div className="bg-C22E2E flex flex-col justify-center items-center h-screen">
+        <img src={Logo} className="h-14 w-auto mr-1 animate-pulse" />
+        <div className="flex justify-center mt-3">
+          <img src={TitleLogo} className="h-3 ml-1 animate-formbounce" />
+        </div>
       </div>
-      {/* <div className="flex justify-center mt-10 font-gexo text-FFF6E4">
-          <div data-aos="fade-right" className="mr-1">
-            {letters.map((letter, index) => (
-              <span
-                key={index}
-                data-aos={animationConfig[index].dataAos}
-                data-aos-delay={animationConfig[index].dataAosDelay}
-              >
-                {letter}
-              </span>
-            ))}
-          </div>
-          <div data-aos="fade-left" className="flex">
-            {letters2.map((letter, index) => (
-              <span
-                key={index}
-                data-aos={animationConfig2[index].dataAos}
-                data-aos-delay={animationConfig2[index].dataAosDelay}
-              >
-                {letter}
-              </span>
-            ))}
-          </div>
-          <div data-aos="fade-right" className="flex animate-bounce ml-1">
-            {letters3.map((letter, index) => (
-              <span
-                key={index}
-                data-aos={animationConfig3[index].dataAos}
-                data-aos-delay={animationConfig3[index].dataAosDelay}
-              >
-                {letter}
-              </span>
-            ))}
-          </div>
-        </div> */}
-    </div>
+    </section>
   );
 };
 
