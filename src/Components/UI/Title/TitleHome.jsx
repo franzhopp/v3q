c277ae42709771ae2a610b58b03d5ace0ddcfcc3
louohs <louisa.hadjili@epitech.eu>
2023-10-28 21:43:29 +0200

    [UPDATE] home and add trad second section

--- src/Components/UI/Title/TitleHome.jsx
+++ src/Components/UI/Title/TitleHome.jsx
@@ -7,7 +7,7 @@ const TitleHome = () => {
   const { language } = useLanguage();
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-my-screen pt-16">
+      <div className="flex flex-col justify-center items-center h-my-screen">
         <h1 className="font-extrabold tracking-tight text-FFF6E4 text-4xl sm:text-8xl md:text-8xl">
           <motion.span
             initial={{ opacity: 0, y: 20 }}
@@ -27,10 +27,10 @@ const TitleHome = () => {
           </span>
         </div>
       </div>
-      <div className="flex justify-center sm:justify-end px-0 sm:px-20 pt-16">
+      <div className="flex justify-center sm:justify-end px-0 sm:px-20">
         <ButtonDiscover />
       </div>
-      <p className="pt-16 pb-10">
+      <p className="pt-16 pb-16">
         <span className="whitespace-nowrap animation-scrolling-rtl text-black text-2xl sm:text-4xl">
           {translations[language].scrollingTextFr}
           {translations[language].scrollingTextEn}
