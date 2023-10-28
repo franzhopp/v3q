770fa24c6ce4337535c8c3a644425fbc53c39418
louohs <louisa.hadjili@epitech.eu>
2023-10-28 18:23:34 +0200

    [ADD] line from modal cookie and bg modal

--- src/Components/Cookie/Cookie.jsx
+++ src/Components/Cookie/Cookie.jsx
@@ -15,7 +15,7 @@ const CookieModal = ({ isOpen, onRequestClose, onAccept }) => {
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       contentLabel="Cookie Modal"
-      className="fixed bg-FFF6E4 shadow-md rounded-tr-2xl rounded-tl-2xl bottom-0 left-0 right-0 p-10 outline-none border-none"
+      className="fixed bg-FFF6E4 shadow-md rounded-tr-2xl rounded-tl-2xl bottom-0 left-0 right-0 p-6 outline-none border-none"
     >
       <div className="flex justify-center pb-5">
         <span className="w-12 h-1 rounded-full bg-gray-400"></span>
