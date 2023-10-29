257d4fbe1e0f0b9b825e6d36de263164f4b490a3
louohs <louisa.hadjili@epitech.eu>
2023-10-29 18:57:48 +0100

    add animation main

--- src/loading.jsx
+++ src/loading.jsx
@@ -2,31 +2,6 @@ import Logo from "./assets/logo-3.png";
 import TitleLogo from "./assets/logo-loading.png";
 
 const Loader = () => {
-  // const text = "navi";
-  // const letters = text.split("");
-  // const animationConfig = [
-  //   { dataAos: "fade-left", dataAosDelay: 100 },
-  //   { dataAos: "fade-left", dataAosDelay: 200 },
-  //   { dataAos: "fade-left", dataAosDelay: 300 },
-  //   { dataAos: "fade-left", dataAosDelay: 400 },
-  //   { dataAos: "fade-left", dataAosDelay: 500 },
-  // ];
-
-  // const text2 = "studio";
-  // const letters2 = text2.split("");
-  // const animationConfig2 = [
-  //   { dataAos: "fade-left", dataAosDelay: 100 },
-  //   { dataAos: "fade-left", dataAosDelay: 200 },
-  //   { dataAos: "fade-left", dataAosDelay: 300 },
-  //   { dataAos: "fade-left", dataAosDelay: 400 },
-  //   { dataAos: "fade-left", dataAosDelay: 500 },
-  //   { dataAos: "fade-left", dataAosDelay: 600 },
-  // ];
-
-  // const text3 = ".";
-  // const letters3 = text3.split("");
-  // const animationConfig3 = [{ dataAos: "fade-left", dataAosDelay: 100 }];
-
   return (
     <section className="overflow-x-hidden">
       <div className="bg-C22E2E flex flex-col justify-center items-center h-screen">
