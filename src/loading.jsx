fb587e2d88ca433f6d8e48181ef37ae6bee741e8
louohs <louisa.hadjili@epitech.eu>
2023-10-30 14:59:11 +0100

    update animation home and loader

--- src/loading.jsx
+++ src/loading.jsx
@@ -4,7 +4,7 @@ import TitleLogo from "./assets/logo-loading.png";
 const Loader = () => {
   return (
     <section className="overflow-x-hidden">
-      <div className="bg-C22E2E flex flex-col justify-center items-center h-screen">
+      <div className="bg-C22E2E flex flex-col justify-center items-center pt-80 overflow-x-hidden">
         <img src={Logo} className="h-14 w-auto mr-1 animate-pulse" />
         <div className="flex justify-center mt-3">
           <img src={TitleLogo} className="h-3 ml-1 animate-formbounce" />
