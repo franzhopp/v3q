30b863be0538755bf778b3f6337ef1f28859bccd
louohs <louisa.hadjili@epitech.eu>
2023-11-02 16:09:21 +0100

    update lines

--- src/Components/Section/SectionAgency.jsx
+++ src/Components/Section/SectionAgency.jsx
@@ -85,7 +85,7 @@ const SectionAgency = () => {
         </div>
       <ButtonAgency />
       </section>
-      <section className="bg-0C3E78 border-t border-b border-gray-400 py-24 px-4 sm:px-6 lg:px-8 border-red-l-b border-red-r-b">
+      <section className="bg-0C3E78 py-24 px-4 sm:px-6 lg:px-8 border-red-l-b border-red-r-b">
         <div className="pt-10 pb-16 flex flex-wrap justify-center items-center space-x-0 sm:space-x-6">
           <div className="w-80 mt-5">
             <p
