deb4fbb9b8394a8269113daa093fd20cb3781ee6
louohs <louisa.hadjili@epitech.eu>
2023-11-12 03:51:55 +0100

    update page portfolio

--- src/Components/Portfolio/AllProjects/Portfolio.jsx
+++ src/Components/Portfolio/AllProjects/Portfolio.jsx
@@ -136,8 +136,8 @@ const Portfolio = () => {
             </div>
           </div>
           <svg
-            width="192"
-            height="193"
+            width="60"
+            height="60"
             viewBox="0 0 192 193"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
