f2ede56167162dcb5efacbac8fce4836d56a6fa8
louohs <louisa.hadjili@epitech.eu>
2023-11-24 21:33:33 +0100

    update svg 5

--- src/Components/Contact/FormContact.jsx
+++ src/Components/Contact/FormContact.jsx
@@ -546,10 +546,10 @@ const Form = ({ label, type, value }) => {
               <div className="flex justify-start relative">
                 <svg
                   // data-aos="fade-up-left"
-                  className="absolute -top-12 -rotate-12 -left-32"
+                  className="absolute -top-1 -rotate-12 -left-16"
                   xmlns="http://www.w3.org/2000/svg"
-                  width="150"
-                  height="150"
+                  width="100"
+                  height="100"
                   viewBox="0 0 215 219"
                   fill="none"
                 >
