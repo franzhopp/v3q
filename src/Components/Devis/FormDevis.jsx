5839c97bb415858fd08a6f375b41ad71e087eb3e
louohs <louisa.hadjili@epitech.eu>
2023-11-24 23:30:36 +0100

    update svg 6

--- src/Components/Devis/FormDevis.jsx
+++ src/Components/Devis/FormDevis.jsx
@@ -714,8 +714,8 @@ const DevisFree = ({ label, type, value }) => {
                   // data-aos="fade-up-left"
                   className="absolute -top-1 -rotate-12 -left-16"
                   xmlns="http://www.w3.org/2000/svg"
-                  width="150"
-                  height="150"
+                  width="100"
+                  height="100"
                   viewBox="0 0 215 219"
                   fill="none"
                 >
