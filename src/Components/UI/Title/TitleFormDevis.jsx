1a72c504d296be9962e8f5ad0fcd367b4cf8ab4e
louohs <louisa.hadjili@epitech.eu>
2023-11-26 16:03:14 +0100

    update pages title

--- src/Components/UI/Title/TitleFormDevis.jsx
+++ src/Components/UI/Title/TitleFormDevis.jsx
@@ -8,7 +8,7 @@ const TitleDevis = () => {
         className="pt-28 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold"
         data-aos="fade-left"
       >
-        <p className="mt-4 mr-5 text-4xl sm:text-6xl">
+        <p className="mt-10 sm:mt-4 mr-5 text-4xl sm:text-6xl">
           {" "}
           {translations[language].navSousFreeFr}
           {translations[language].navSousFreeEn}
--- src/Components/UI/Title/TitleRate.jsx
+++ src/Components/UI/Title/TitleRate.jsx
@@ -5,7 +5,7 @@ const TitleRate = () => {
         className="pt-28 text-FFF6E4 sm:ml-44 sm:text-left text-center font-extrabold"
         data-aos="fade-left"
       >
-        <p className="mt-7 sm:mt-4 mr-5 text-4xl sm:text-6xl">Tarification</p>
+        <p className="mt-10 sm:mt-4 mr-5 text-4xl sm:text-6xl">Tarification</p>
       </div>
     </div>
   );
