096f07d5807ce97eb65071e9dbb02b2f5fa94858
louohs <louisa.hadjili@epitech.eu>
2023-12-12 23:48:21 +0100

    update page melissa

--- src/Components/UI/Avatar/TemplateAvatarDesigner.jsx
+++ src/Components/UI/Avatar/TemplateAvatarDesigner.jsx
@@ -24,7 +24,22 @@ const AvatarDesigner = () => {
         <LoaderAvatar />
       ) : (
         <div className="flex justify-center">
-          <div className="bg-FFF6E4 absolute rounded-full h-48 w-48 sm:h-56 sm:w-56 left-10 sm:left-96 top-52 sm:top-56 sm:ml-4 mb-4">
+          <div className="flex justify-end relative">
+            <svg
+              className="absolute bottom-20 left-96"
+              xmlns="http://www.w3.org/2000/svg"
+              width="79"
+              height="79"
+              viewBox="0 0 79 79"
+              fill="none"
+            >
+              <path
+                d="M39.5 0L50.1686 28.8314L79 39.5L50.1686 50.1686L39.5 79L28.8314 50.1686L0 39.5L28.8314 28.8314L39.5 0Z"
+                fill="#FFF6E4"
+              />
+            </svg>
+          </div>
+          <div className="bg-1E1E20 absolute rounded-full h-48 w-48 sm:h-56 sm:w-56 left-10 sm:left-96 top-52 sm:top-56 sm:ml-4 mb-4">
             <img
               src={AvatarMelissa}
               className="rounded-full p-3"
