6024a283e8593bd2cd0cb8ce988318e0cdf2b3df
louohs <louisa.hadjili@epitech.eu>
2024-01-22 04:17:18 +0100

    update

--- src/Components/UI/Avatar/TemplateAvatarWebDeveloper.jsx
+++ src/Components/UI/Avatar/TemplateAvatarWebDeveloper.jsx
@@ -22,7 +22,7 @@ const Avatar = () => {
         <LoaderAvatar />
       ) : ( */}
       <div className="flex justify-center">
-        <div className="bg-FFF6E4 absolute rounded-full h-48 w-48 sm:h-56 sm:w-56 left-10 xl:left-96 top-72 xl:top-64 sm:ml-4 mb-4">
+        <div className="bg-FFF6E4 absolute rounded-full h-48 w-48 sm:h-56 sm:w-56 left-10 2xl:left-96 top-72 xl:top-64 sm:ml-4 mb-4">
           <img src={ImageShoto} className="rounded-full p-3" alt="Avatar" />
         </div>
       </div>
