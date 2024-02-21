a6daa673f81dbe38b2408f2132407d4c01f59b6e
louohs <louisa.hadjili@epitech.eu>
2024-02-21 17:13:29 +0100

    update

--- src/Components/UI/Form/Contact/Svg/SvgFontRight.jsx
+++ src/Components/UI/Form/Contact/Svg/SvgFontRight.jsx
@@ -1,7 +1,7 @@
 const SvgFontRight = () => {
   return (
     <svg
-      className="absolute top-20 -right-24"
+      className="absolute top-20 -right-24 animate-spin"
       xmlns="http://www.w3.org/2000/svg"
       width="150"
       height="150"
