3760fc37136f15eb4e2d6f7b70719417d02f8167
louohs <louisa.hadjili@epitech.eu>
2024-02-29 14:15:46 +0100

    update page dev

--- src/Pages/Discover/DiscoverDevelopper/Copyright/LocationCopyright.jsx
+++ src/Pages/Discover/DiscoverDevelopper/Copyright/LocationCopyright.jsx
@@ -1,7 +1,7 @@
 const Copyright = () => {
   return (
     <p className={`text-gray-300 flex justify-center ml-16 text-xs -rotate-12`}>
-      📸 : Palais Grand-Ducal, 1728 Luxembourg / &copy; Astres Lunaires
+      📸 : Palais Grand-Ducal, 1728 Luxembourg / &copy; Louisa
     </p>
   );
 };
