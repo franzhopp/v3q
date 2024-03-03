53f00605c152687333e6ace6ede93d290190ab99
louohs <louisa.hadjili@epitech.eu>
2024-03-03 23:24:37 +0100

    update

--- src/Components/Section/TextStrategy/Title.jsx
+++ src/Components/Section/TextStrategy/Title.jsx
@@ -6,7 +6,7 @@ const TitleStrategy = () => {
   return (
     <h1
       className="text-fff6e4 text-center lg:text-left text-3xl lg:text-4xl font-extrabold px-12 lg:px-0"
-      // data-aos="fade-right"
+      data-aos="fade-right"
     >
       {translations[language].titleSecondSectionFr}
       {translations[language].titleSecondSectionEn}
