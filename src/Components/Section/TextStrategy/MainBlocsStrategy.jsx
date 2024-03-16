8b8522cfb1df3b4d86b2302cc10ff4d64f230e6f
louohs <louisa.hadjili@epitech.eu>
2024-03-16 02:33:25 +0100

    update text strategy

--- src/Components/Section/TextStrategy/MainBlocsStrategy.jsx
+++ src/Components/Section/TextStrategy/MainBlocsStrategy.jsx
@@ -4,7 +4,7 @@ import SubTextStrategy from "../TextStrategy/SubText.jsx";
 const MainBlocsStrategy = () => {
   return (
     <section x-comp={`Main`} className="bg-c22e2e flex justify-center py-36">
-      <div className={`px-10`}>
+      <div className={`px-24`}>
         <TitleStrategy />
         <div className="flex flex-wrap justify-center items-center space-x-0 lg:space-x-6">
           <SubTextStrategy />
